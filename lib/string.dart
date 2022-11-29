147314d60d5e44eea59e28fd73a6e3a5de42b311
Liplum <Li_plum@outlook.com>
2022-11-30 00:50:35 +0800

    listview

--- lib/string.dart
+++ lib/string.dart
@@ -34,29 +34,4 @@ extension StringEx on String {
           textWidthBasis: textWidthBasis,
           textHeightBehavior: textHeightBehavior,
           selectionColor: selectionColor);
-
-  TextButton textButton({
-    Key? key,
-    VoidCallback? onPressed,
-    VoidCallback? onLongPress,
-    ValueChanged<bool>? onHover,
-    ValueChanged<bool>? onFocusChange,
-    ButtonStyle? style,
-    FocusNode? focusNode,
-    bool autofocus = false,
-    Clip clipBehavior = Clip.none,
-    MaterialStatesController? statesController,
-  }) =>
-      TextButton(
-          key: key,
-          onPressed: onPressed,
-          onLongPress: onLongPress,
-          onHover: onHover,
-          onFocusChange: onFocusChange,
-          style: style,
-          focusNode: focusNode,
-          autofocus: autofocus,
-          clipBehavior: clipBehavior,
-          statesController: statesController,
-          child: Text(this));
 }
