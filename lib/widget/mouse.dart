00b87c5499018d35d609fe8137007a9a94951cd6
Liplum <Li_plum@outlook.com>
2022-12-17 08:53:29 +0800

    MouseRegion

---
+++ lib/widget/mouse.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+extension MouseEx on Widget {
+  MouseRegion mouseRegion({
+    PointerEnterEventListener? onEnter,
+    PointerHoverEventListener? onHover,
+    PointerExitEventListener? onExit,
+    bool opaque = true,
+    HitTestBehavior? hitTestBehavior,
+    MouseCursor cursor = MouseCursor.defer,
+    Key? key,
+  }) =>
+      MouseRegion(
+        onEnter: onEnter,
+        onHover: onHover,
+        onExit: onExit,
+        opaque: opaque,
+        hitTestBehavior: hitTestBehavior,
+        child: this,
+      );
+
+  MouseRegion withCursor(
+    MouseCursor cursor, {
+    Key? key,
+  }) =>
+      MouseRegion(
+        cursor: cursor,
+        child: this,
+      );
+}
