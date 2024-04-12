4d635d13a7047592f1f7de096c55754a9b2221ac
Liplum <Li_plum@outlook.com>
2024-04-12 15:29:54 +0800

    inFilledCard & inOutlinedCard

--- rettulf/lib/widget/material.dart
+++ rettulf/lib/widget/material.dart
@@ -27,6 +27,58 @@ extension RettulfMaterialX on Widget {
         child: this,
       );
 
+  /// see [Card.filled]
+  Card inFilledCard({
+    Key? key,
+    Color? color,
+    Color? shadowColor,
+    Color? surfaceTintColor,
+    double? elevation,
+    ShapeBorder? shape,
+    bool borderOnForeground = true,
+    EdgeInsetsGeometry? margin,
+    Clip? clip,
+    bool semanticContainer = true,
+  }) =>
+      Card.filled(
+        key: key,
+        color: color,
+        shadowColor: shadowColor,
+        surfaceTintColor: surfaceTintColor,
+        elevation: elevation,
+        shape: shape,
+        borderOnForeground: borderOnForeground,
+        margin: margin,
+        clipBehavior: clip,
+        child: this,
+      );
+
+  /// see [Card.outlined]
+  Card inOutlinedCard({
+    Key? key,
+    Color? color,
+    Color? shadowColor,
+    Color? surfaceTintColor,
+    double? elevation,
+    ShapeBorder? shape,
+    bool borderOnForeground = true,
+    EdgeInsetsGeometry? margin,
+    Clip? clip,
+    bool semanticContainer = true,
+  }) =>
+      Card.outlined(
+        key: key,
+        color: color,
+        shadowColor: shadowColor,
+        surfaceTintColor: surfaceTintColor,
+        elevation: elevation,
+        shape: shape,
+        borderOnForeground: borderOnForeground,
+        margin: margin,
+        clipBehavior: clip,
+        child: this,
+      );
+
   /// see [InkWell]
   InkWell inkWell({
     Key? key,
